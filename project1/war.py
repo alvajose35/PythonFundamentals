236ddb0c19fc8bcaff901a1285910603dbca8c36
Jose V <alvajose35@gmail.com>
2025-02-07 09:41:04 -0500

    Check winner function

--- project1/war.py
+++ project1/war.py
@@ -1,6 +1,6 @@
 import random
 
-# Function: return card value
+# Functions
 def cardValue(card):
 
 	if card[0] == '2' or card[0] == '3' or card[0] == '4' or card[0] == '5' or card[0] == '6' or card[0] == '7' or card[0] == '8' or card[0] == '9':
@@ -20,15 +20,23 @@ def cardValue(card):
 	else:
 		return "ERROR"
 
-# Declare variables & Create a deck of cards
-'''deck = [
-	"2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "TS", "JS", "QS", "KS", "AS",
-	"2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "TH", "JH", "QH", "KH", "AH",
-	"2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "TD", "JD", "QD", "KD", "AD",
-	"2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "TC", "JC", "QC", "KC", "AC",
-	"XB", "XR"
-]'''
+def checkGameOver():
 
+	# Assign global variables
+	global comp_deck, user_deck, comp_pile, user_pile
+	
+	if (len(comp_deck) + len(comp_pile)) == 0:
+		print("Game Over. User wins!! (Computer lost)")
+		return True
+	
+	elif len(user_deck) + len(user_pile) == 0:
+		print("Game Over. Computer wins!! (User lost)")
+		return True
+	
+	else:
+		return False
+
+# Declare variables & Create a deck of cards
 deck = [
 	"2♠️", "3♠️", "4♠️", "5♠️", "6♠️", "7♠️", "8♠️", "9♠️", "T♠️", "J♠️", "Q♠️", "K♠️", "A♠️", 
 	"2♥️", "3♥️", "4♥️", "5♥️", "6♥️", "7♥️", "8♥️", "9♥️", "T♥️", "J♥️", "Q♥️", "K♥️", "A♥️",
@@ -42,50 +50,40 @@ user_pile = []
 
 dealHand = True
 
+tempDeckComp = ["2♠️", "3♠️", "4♠️", "5♠️", "6♠️"]
+tempDeckUser = ["T♦️", "J♦️", "Q♦️", "K♦️", "A♦️"]
+
 # Shuffle cards and plit two piles
 random.shuffle(deck)
 
-comp_deck = deck[:27]
-user_deck = deck[27:]
+# comp_deck = deck[:27]
+# user_deck = deck[27:]
+
+comp_deck = tempDeckComp
+user_deck = tempDeckUser
+
 
 print(comp_deck)
 print(user_deck)
 
-# print("----")
-# print(comp_deck[-1])
-# print(user_deck[-1])
-# print("----")
-
-'''comp_card = comp_deck.pop()
-user_card = user_deck.pop()
-
-print(comp_card)
-print(user_card)'''
-
-# print(comp_deck)
-# print(user_deck)
-
-'''comp_card_v = cardValue(comp_card)
-user_card_v	= cardValue(user_card)'''
-
-
 while dealHand:
+
+	# Check status
+	gameOver = checkGameOver()
+	if gameOver:
+		break
 	
 	comp_card_v = cardValue(comp_deck[-1])
 	user_card_v	= cardValue(user_deck[-1])
 
-	print(comp_deck[-1], comp_card_v)
-	print(user_deck[-1], user_card_v)
-
-	# print(comp_card_v)
-	# print(user_card_v)
-
-
 	# PRINT SCREEN
 	print("---------------------------------------")
 
-	print("COMP: [" + str(len(comp_deck) - 1) + "]  " + comp_deck[-1])     # print("COMP: [" + str(len(comp_deck)) + "]  " + comp_card)
-	print("USER: [" + str(len(user_deck) - 1) + "]  " + user_deck[-1])     # print("USER: [" + str(len(user_deck)) + "]  " + user_card)
+	print(comp_deck[-1], comp_card_v)
+	print(user_deck[-1], user_card_v)
+
+	print("COMP: [" + str(len(comp_deck) - 1) + "]  " + comp_deck[-1])
+	print("USER: [" + str(len(user_deck) - 1) + "]  " + user_deck[-1])
 
 	if comp_card_v > user_card_v:
 		print("\nComputer Wins!\n")
@@ -98,15 +96,6 @@ while dealHand:
 	print("Computer's Pile:", comp_pile)
 	print("User's Pile:", user_pile)
 
-	'''if comp_card_v > user_card_v:
-		comp_pile.append(comp_card)
-		comp_pile.append(user_card)
-	elif comp_card_v < user_card_v:
-		user_pile.append(comp_card)
-		user_pile.append(user_card)
-	else:
-		print("WAR GAME")'''
-
 	if comp_card_v > user_card_v:
 		comp_pile.append(comp_deck.pop())
 		comp_pile.append(user_deck.pop())
@@ -118,9 +107,4 @@ while dealHand:
 
 	input()
 
-print("Computer's Pile:", comp_pile)
-print("User's Pile:", user_pile)
-print(comp_deck)
-print(user_deck)
-
-
+print("SHUTING OFF")
